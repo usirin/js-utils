7bb7bdd42c55568e5e528541be714e03bdc8e1df
usirin <umutsirin1@gmail.com>
2014-02-13 14:22:58 -0500

    initial commit

---
+++ NumberUtils.js
@@ -0,0 +1,31 @@
+Number.prototype.floor = function() {
+  return Math.floor(this);
+};
+
+Number.prototype.ceil = function() {
+  return Math.ceil(this);
+};
+
+Number.prototype.round = function() {
+  return Math.round(this);
+};
+
+Number.prototype.sqrt = function() {
+  return Math.sqrt(this);
+};
+
+Number.prototype.powerOf = function(power) {
+  return Math.pow(this, power);
+};
+
+Number.prototype.log = function() {
+  return Math.log(this);
+};
+
+Number.prototype.abs = function() {
+  return Math.abs(this);
+};
+
+Number.prototype.exp = function() {
+  return Math.exp(this);
+};
